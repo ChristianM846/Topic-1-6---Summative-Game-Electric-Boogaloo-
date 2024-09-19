325f19f7b80bf74a6f194d50de7ff142e9696001
Christian Moyes <339463846@GoTVDSB.ca>
2024-09-19 09:05:33 -0400

    Beginning

--- Program.cs
+++ Program.cs
@@ -4,7 +4,51 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double money = 100;
+            double bet;
+            int betChoice;
+            bool done = false;
+
+            Console.WriteLine("Welcome to the Dice Game table");
+            Console.WriteLine($"I see you're buying in with {money.ToString("C")}, very well, have a seat.");
+            Console.WriteLine("Press ENTER to have a seat:");
+            Console.ReadLine();
+            Console.WriteLine("Thank you. Now, let me explain the game.");
+            Thread.Sleep(1000);
+            Console.WriteLine("You will place a bet, trying to guess what the result of a roll of two dice will be, from a provided list");
+            Console.WriteLine("If you get it right, you win some money. If you get it wrong I keep your bet.");
+            Thread.Sleep(1000);
+            Console.WriteLine("Now, press ENTER to begin the game.");
+            Console.ReadLine();
+
+            while (!done)
+            {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
