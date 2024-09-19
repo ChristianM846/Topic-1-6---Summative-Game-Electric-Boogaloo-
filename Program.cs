e1c3ed5890ddf0027ddacac74d897aed79f41a63
Christian Moyes <339463846@GoTVDSB.ca>
2024-09-19 08:50:35 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic_1_6___Summative_Game__Electric_Boogaloo_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
